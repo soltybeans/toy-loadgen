a28f95ca17c6f0e8305b8cb1da239ea6fbd89118
soltybeans <soltybeans@gmail.com>
2024-06-30 01:21:46 +0200

    feat(hyper): HTTP2 connection established
    Use hyper-util's legacy client builder
    semantics for establishing an http2 only
    connection. Tested with podinfo with h2c
    enabled. May circle back to this and use
    the connection module for http2 in hyper-1.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::fmt::{Error};
+use std::time::{Duration, Instant};
 use clap::Parser;
+use http_body_util::{BodyExt, Full};
+
+use hyper::{Method, Request};
+use hyper::body::{Bytes};
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::{TokioExecutor, TokioTimer};
+
 
 #[derive(Parser, Debug)]
-#[command( about, long_about = None)]
+#[command(about, long_about = None)]
 struct TestParams {
     /// Fixed call rate (per second)
     #[arg(short, long)]
@@ -11,13 +20,60 @@ struct TestParams {
     #[arg(short, long)]
     total: u32,
 
-    /// Address of the form <endpoint>:<port>
-    address: String
+    /// Address of the form <endpoint>:<port>. Example: nghttp2.org:80
+    address: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    // CLI check
     let args = TestParams::parse();
     println!("Rate is {}", args.rate);
     println!("Total is {}", args.total);
     println!("Address is {}", args.address);
+
+    // Validation
+    let address = args.address;
+    validate_address(address.as_str().trim())?;
+
+    // Hyper-Client - _SINGLE_ connection opening
+    // ASSUMPTION - we will use ONE TCP connection to execute N concurrent requests for the host:port.
+    // ASSUMPTION - the application supports h2c i.e. ALPN not required (TLS with H2 not used).
+    // i.e. nghttp2.org:80 or podinfo with h2c.enabled = true
+    let client = Client::builder(TokioExecutor::new())
+        .pool_idle_timeout(Duration::from_secs(30))
+        .pool_timer(TokioTimer::new())
+        .http2_only(true)
+        .build_http();
+
+    let req: Request<Full<Bytes>> = Request::builder()
+        .method(Method::GET)
+        .uri(format!("http://{}", address))
+        .body(Full::from(" "))
+        .expect("establishing connection failed!");
+
+    // This part needs to be concurrent
+    let start_time = Instant::now();
+    let future = client.request(req);
+    let res = future.await.unwrap();
+    let (parts, body) = res.into_parts();
+    println!("Response status is: {:?}", parts.status);
+    let data = body.collect().await.unwrap();
+    let elapsed_time = start_time.elapsed().as_micros();
+    println!("Response is : {:?}", data);
+    println!("[elapsed time] : {}", elapsed_time);
+
+    // TODO: Time the request-response (stream end) and make sure to capture results.
+    // TODO: Conditionally update error rate
+
+    Ok(())
+}
+
+fn validate_address(address: &str) -> Result<(), Error> {
+    let parts: Vec<&str> = address.split(':').collect();
+    if parts[1].parse::<u32>().is_err() {
+        println!("Specified port is invalid!");
+    }
+    Ok(())
 }
