600b8fb73a6e08db84f5d65d3d843099afa0e10a
soltybeans <soltybeans@gmail.com>
2024-06-29 13:51:08 +0200

    feat(cli): Added CLI params via clap
    Use the `clap` crate to help set up all
    the necessary boilerplate for capturing
    inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command( about, long_about = None)]
+struct TestParams {
+    /// Fixed call rate (per second)
+    #[arg(short, long)]
+    rate: u32,
+
+    /// Maximum number of requests
+    #[arg(short, long)]
+    total: u32,
+
+    /// Address of the form <endpoint>:<port>
+    address: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = TestParams::parse();
+    println!("Rate is {}", args.rate);
+    println!("Total is {}", args.total);
+    println!("Address is {}", args.address);
 }
