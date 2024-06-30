8a13956f841139dc33302d121a84794117a1d83b
soltybeans <soltybeans@gmail.com>
2024-06-30 22:20:10 +0200

    feat(result processing): Print p50 duration and success rate
    Result aggregation now supported after
    all calls made.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use hyper_util::client::legacy::Client;
 use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::rt::{TokioExecutor, TokioTimer};
 use tokio::sync::{mpsc, Mutex};
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{Sender, UnboundedSender};
 use tokio::task::JoinHandle;
 use tokio::time;
 use tokio::time::Interval;
@@ -31,9 +31,15 @@ struct TestParams {
     address: String,
 }
 
-struct UpstreamResult {
-    success_code: bool,
-    p50_response_time_micros: f32,
+struct Workload {
+    rate: u32,
+    address: &'static Arc<String>,
+    client: Client<HttpConnector, Full<Bytes>>,
+    total_calls: &'static Arc<Mutex<u32>>,
+    tx: Sender<()>,
+    tx_result_status_codes: UnboundedSender<u16>,
+    tx_result_duration_micros: UnboundedSender<u128>,
+    time_interval: &'static mut Interval,
 }
 
 
@@ -44,16 +50,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args = TestParams::parse();
     println!("Rate is {}", args.rate);
     println!("Total is {}", args.total);
-    println!("Address is {}", args.address);
 
     // Validation
     let address = args.address;
     validate_address(address.as_str().trim())?;
     let address = Arc::new(address);
-    if args.total < args.rate {
-        println!("Please specify a `total` value that is >= `rate` ");
-        return Ok(());
-    }
 
     let client = Client::builder(TokioExecutor::new())
         .pool_idle_timeout(Duration::from_secs(30))
@@ -67,58 +68,99 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // We use a channel and wait for a single message that signals we've reached our call limit.
     let (tx, rx) = mpsc::channel::<()>(1);
 
+    // Another two channels will be used solely for capturing raw results (status_code and duration).
+    // Their results will be collected into their respective vectors.
+    let (tx_result_status_codes, mut rx_status_codes) = mpsc::unbounded_channel::<u16>();
+    let (tx_result_duration_micros, mut rx_durations) = mpsc::unbounded_channel::<u128>();
+    let mut result_errors: Vec<u16> = vec![];
+    let mut result_durations: Vec<u128> = vec![];
+
     // We need to sustain the call rate, therefore we use tokio's interval.
     let mut time_interval = time::interval(Duration::from_micros(1_000_000));
     time_interval.tick().await; // the first tick is immediate.
     while rx.is_empty() {
-        sustain_call_rate(args.rate, &address, client.clone(), &total_calls, tx.clone(), &mut time_interval).await.unwrap();
+        sustain_call_rate(args.rate, &address, client.clone(), &total_calls, tx.clone(), tx_result_status_codes.clone(), tx_result_duration_micros.clone(), &mut time_interval).await.unwrap();
     }
+
+    // Result processing
+    while !rx_status_codes.is_empty() {
+        result_errors.push(rx_status_codes.recv().await.unwrap());
+    }
+    while !rx_durations.is_empty() {
+        result_durations.push(rx_durations.recv().await.unwrap());
+    }
+
+    process_results(result_durations, result_errors).await;
 
     Ok(())
 }
 
-async fn sustain_call_rate(rate: u32, address: &Arc<String>, client: Client<HttpConnector, Full<Bytes>>, total_calls: &Arc<Mutex<u32>>, tx: Sender<()>, time_interval: &mut Interval) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn process_results(mut result_durations: Vec<u128>, mut result_errors: Vec<u16>) {
+    result_durations.sort_unstable();
+    result_errors.sort_unstable();
+
+    let mut total_5xx_responses = 0;
+    result_errors.iter().for_each(|value| {
+        if value > &499_u16 && value < &599_u16 {
+            total_5xx_responses += 1;
+        }
+    });
+    let success_rate: f32 = ((1 - (total_5xx_responses / result_errors.len())) * 100) as f32;
+    let median = result_durations.len() / 2;
+    println!("success: {} %", success_rate);
+    println!("median (microseconds): {}", result_durations[median]);
+}
+
+
+async fn sustain_call_rate(
+    rate: u32,
+    address: &Arc<String>,
+    client: Client<HttpConnector, Full<Bytes>>,
+    total_calls: &Arc<Mutex<u32>>,
+    tx: Sender<()>,
+    tx_result_status_codes: UnboundedSender<u16>,
+    tx_result_duration_micros: UnboundedSender<u128>,
+    time_interval: &mut Interval) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut join_handles: Vec<JoinHandle<()>> = vec![];
-    // This is the _rate_ that needs to be sustained UNTIL the `total` calls have been made.
-    // We use the rate to bound the number of concurrent tasks
     for _ in 0..rate {
         let calls = Arc::clone(total_calls);
-        let client_conn = client.clone(); // this does _NOT_ need to be locked - we WANT multiple, concurrent requests.
+        let client_conn = client.clone();
         let addr = Arc::clone(address);
-        let tx_for_task = tx.clone();
+        let tx_end_check = tx.clone();
+        let tx_status = tx_result_status_codes.clone();
+        let tx_duration = tx_result_duration_micros.clone();
         join_handles.push(tokio::spawn(async move {
             // Make sure we're within `total` limit - strong consistency needed here hence Mutex
             let mut job_number = calls.lock().await;
             if *job_number > 0 {
                 *job_number -= 1;
             } else {
-                if (tx_for_task.send(()).await).is_ok() {
-                    println!("Total call limit reached!");
+                if (tx_end_check.send(()).await).is_ok() {
+                    println!("Total call limit reached...");
                 }
                 return;
             }
 
-            // Would have been nice to re-use the request object created from _outside_ this task
             let request: Request<Full<Bytes>> = Request::builder()
                 .method(Method::GET)
                 .uri(format!("http://{}", addr))
                 .body(Full::from(" "))
                 .expect("error constructing request!");
 
-            // For the 1 RPS case - we need to make sure 1 second has passed before the next request.
-            let mut time_interval = time::interval(Duration::from_micros(1_000_000));
-            time_interval.tick().await; // the first tick is immediate.
-
             let start_time = Instant::now();
             let future = client_conn.request(request);
             let res = future.await.unwrap();
             let (parts, body) = res.into_parts();
-            let data = body.collect().await.unwrap();
+            // Data is not needed, but body.collect() is useful to stream the FULL response.
+            let _data = body.collect().await.unwrap();
             let elapsed_time_micros = start_time.elapsed().as_micros();
-            println!("Response status is: {:?}", parts.status);
-            // TODO: Do data capture: Store error rate duration
+
+            tx_status.send(parts.status.as_u16()).expect("cannot send status_code!");
+            tx_duration.send(elapsed_time_micros).expect("cannot send duration!");
         }));
     }
+
+    // Use the pre-determined interval and tick preserve the call rate.
     time_interval.tick().await;
 
     // Technically - we may choose to wait for all spawned tasks to complete even if total call limit is reached.
